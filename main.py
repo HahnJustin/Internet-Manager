6e2fadac10f0ba9bf84ff5d2758c3c0d72dc1a14
Justin Hahn <43224702+HahnJustin@users.noreply.github.com>
2024-03-22 15:28:20 -0400

    Added internet on time, and enabling manager in proper state

--- main.py
+++ main.py
@@ -17,9 +17,9 @@ ETHERNET_CMD_BASE = 'netsh interface set interface {}'
 WIFI_CMD_ENABLE = WIFI_CMD_BASE.format('enable')
 WIFI_CMD_DISABLE = WIFI_CMD_BASE.format('disable')
 
-COLOR_AMOUNT = 1000
-COLORS = [(224, 0, 0), (8, 135, 4),(0, 0, 255),(0, 0, 0)]
-#COLORS = [(30, 198, 244), (99, 200, 72),(120, 50, 80),(200, 90, 140)]
+COLOR_AMOUNT = 100
+SHUTDOWN_COLORS = [(224, 0, 0), (8, 135, 4),(0, 0, 255),(0, 0, 0)]
+UP_COLORS = [(0, 144, 227), (0, 135, 90),(0, 0, 255),(0, 0, 0)]
 
 JSON_FILE = "storage.json"
 SECONDS_DATA_UPDATE = 60
@@ -35,29 +35,40 @@ ASSET_FOLDER = "assets"
 global now
 global time_since_json_write
 global colors
-global shutdown_labels
+global time_action_labels
 global json_data
 global date_label
 global time_label
 
-class shutdown_data():
+class time_action_data():
     def __init__(self, time, label):
         self.datetime = time
         self.label = label
 
     def update(self):
+        a = 1
+
+    def update_helper(self, color_list):
         global now
-        global colors
+        global shutdown_colors
         delta = self.datetime - now
         if self.datetime <= now:
             color_index = 0
         else:
             hour_delta = int(delta.total_seconds() ** 1.4) / int(60 * 60)
             color_index = clamp(int(hour_delta * (COLOR_AMOUNT / 100)), 0, COLOR_AMOUNT-1)
-        bg_color = Color('#%02x%02x%02x' % tuple(map(round, colors[color_index])))
+        bg_color = Color('#%02x%02x%02x' % tuple(map(round, color_list[color_index])))
         time_left = ':'.join(str(delta).split(':')[:2])
         date_time = self.datetime.strftime("%H:%M:%S")
         self.label.configure(text=f"{date_time} | {time_left}", bg_color=bg_color.get_hex())
+
+class shutdown_data(time_action_data):
+    def update(self):
+        super().update_helper(shutdown_colors)
+
+class internet_up_data(time_action_data):
+    def update(self):
+        super().update_helper(up_colors)
 
 def clamp(n, smallest, largest): return max(smallest, min(n, largest))
 
@@ -73,7 +84,7 @@ def every(delay, task):
 
 def update_datetime_labels():
     global time_since_json_write
-    global shutdown_labels
+    global time_action_labels
     global json_data
     global now
     global date_label
@@ -83,7 +94,7 @@ def update_datetime_labels():
     date_label.configure(text=date)
     time_label.configure(text=time)
 
-    soonest_time_delta = shutdown_labels[0].datetime  - now
+    soonest_time_delta = time_action_labels[0].datetime  - now
     time_until_label.configure(text=':'.join(str(soonest_time_delta).split(':')[:2]))
 
     now = datetime.now()
@@ -104,17 +115,22 @@ def update_datetime_labels():
         sort_labels()
 
     # shutdown label maintenance
-    for data in shutdown_labels:
+    for data in time_action_labels:
         data.update()
 
         # actual shutdown
-        if data.datetime <= now:
-           recalculate_time(data)
-           turn_off_wifi()
-           turn_off_ethernet()
-           sort_labels()
+        if data.datetime > now:
+            continue
+        elif type(data) is shutdown_data:
+            turn_off_wifi()
+            turn_off_ethernet()
+        elif type(data) is internet_up_data:
+            turn_on_wifi()
+            turn_on_ethernet()
+        recalculate_time(data)
+        sort_labels()
 
-def recalculate_time(data: shutdown_data):
+def recalculate_time(data: time_action_data):
    data.datetime = data.datetime.replace(day=tomorrow.day)
    data.update()
 
@@ -136,13 +152,21 @@ def turn_off_wifi():
 def turn_on_ethernet():
     for ethernet in cfg["ethernet"]:
         execute_cmd(CMD_BASE.format(ETHERNET_CMD_BASE.format(ethernet) + " enabled"))
-    status_label.pack_forget()
+    status_label.configure(text="Internet Activation Triggered", fg_color="blue")
+    status_label.pack()
+    run_function_in_minute(lambda: status_label.pack_forget())
 
 def turn_off_ethernet():
     for ethernet in cfg["ethernet"]:
         execute_cmd(CMD_BASE.format(ETHERNET_CMD_BASE.format(ethernet) + " disabled"))
     status_label.configure(text="Internet Shutdown Triggered", fg_color="red")
     status_label.pack()
+    run_function_in_minute(lambda: status_label.pack_forget())
+
+def run_function_in_minute(func):
+    thread = threading.Timer(60.0, func) # 60 seconds = 1 minute
+    thread.daemon = True
+    thread.start()
 
 def write_data_to_json():
     with open(JSON_FILE, 'w') as f:
@@ -154,20 +178,20 @@ def string_now():
 def LerpColour(c1,c2,t):
     return (c1[0]+(c2[0]-c1[0])*t, c1[1]+(c2[1]-c1[1])*t, c1[2]+(c2[2]-c1[2])*t)
 
-def create_gradient():
+def create_gradient(colors):
     gradient = []
-    for i in range(len(COLORS)-2):
+    for i in range(len(colors)-2):
         for j in range(COLOR_AMOUNT):
-            gradient.append(LerpColour(COLORS[i],COLORS[i+1],j/COLOR_AMOUNT))
+            gradient.append(LerpColour(colors[i],colors[i+1],j/COLOR_AMOUNT))
     return gradient
 
 def sort_labels():
-    global shutdown_labels
-    shutdown_labels = sorted(shutdown_labels, key=lambda x: x.datetime)
-    for data in shutdown_labels:
+    global time_action_labels
+    time_action_labels = sorted(time_action_labels, key=lambda x: x.datetime)
+    for data in time_action_labels:
         data.label.pack_forget()
 
-    for data in shutdown_labels:
+    for data in time_action_labels:
         data.label.pack(ipadx=10, ipady=10, fill="both")
 
 def get_streak_icon(streak : int) -> ImageTk:
@@ -197,6 +221,21 @@ def update_streak_graphics():
 def streak_mod(mod : int):
     json_data[STREAK_KEY] += mod
     update_streak_graphics()
+
+def time_action_label_create(time : str, shutdown : bool):
+    target = datetime.strptime(time, '%H:%M:%S')
+    target = target.replace(year=now.year, month=now.month, day=now.day)
+
+    if now > target:
+      target = target.replace(day=tomorrow.day)
+
+    shutdown_time_label = customtkinter.CTkLabel(widget_frame, text=target)
+    shutdown_time_label.pack(ipadx=10, ipady=10, fill="both")
+
+    if shutdown:
+        time_action_labels.append(shutdown_data(target, shutdown_time_label))
+    else:
+        time_action_labels.append(internet_up_data(target, shutdown_time_label))
 
 # Defining basic time variables
 now = datetime.now()
@@ -246,22 +285,20 @@ bottom_frame = customtkinter.CTkFrame(app)
 bottom_frame.pack(side='bottom', fill="x")
 
 # Making shutdown label color gradient
-colors = create_gradient()
+shutdown_colors = create_gradient(SHUTDOWN_COLORS)
+up_colors = create_gradient(UP_COLORS)
 
-shutdown_labels = []
+time_action_labels = []
 
 # Intializing shutdown labels
 for shutdown_time in cfg["shutdowntimes"]:
-    target = datetime.strptime(shutdown_time, '%H:%M:%S')
-    target = target.replace(year=now.year, month=now.month, day=now.day)
+    time_action_label_create(shutdown_time, True)
 
-    if now > target:
-      target = target.replace(day=tomorrow.day)
+# Intializing internet up labels
+for up_time in cfg["uptimes"]:
+    time_action_label_create(up_time, False)
 
-    shutdown_time_label = customtkinter.CTkLabel(widget_frame, text=target)
-    shutdown_time_label.pack(ipadx=10, ipady=10, fill="both")
-
-    shutdown_labels.append(shutdown_data(target, shutdown_time_label))
+sort_labels()
 
 # Adding Time
 date_label = customtkinter.CTkLabel(app, text="Date", font=("Arial", 20))
@@ -276,6 +313,14 @@ time_until_label.pack()
 status_label = customtkinter.CTkLabel(app, text=f"" )
 status_label.pack_forget()
 
+# Calculating what state the manager should be in:
+if type(time_action_labels[0]) is shutdown_data:
+    turn_off_wifi()
+    turn_off_ethernet()
+elif type(time_action_labels[0]) is internet_up_data:
+    turn_on_wifi()
+    turn_on_ethernet()
+
 # Debug turn internet on and off buttons
 if cfg[DEBUG_KEY]:
     debug_frame = customtkinter.CTkFrame(app)
@@ -285,15 +330,15 @@ if cfg[DEBUG_KEY]:
                             command = turn_off_ethernet) 
     turn_on = customtkinter.CTkButton(debug_frame, text = 'Turn on Internet',
                             command = turn_on_ethernet)  
-    turn_off.pack(side = 'left', anchor='e', expand=True)
-    turn_on.pack(side='left', anchor='w', expand=True)
+    turn_off.pack(side = 'left', anchor='e', expand=False)
+    turn_on.pack(side='left', anchor='w', expand=False)
 
     streak_inc = customtkinter.CTkButton(debug_frame, text = 'Streak +',
                             command = lambda : streak_mod(30)) 
     streak_dec = customtkinter.CTkButton(debug_frame, text = 'Streak -',
                             command = lambda : streak_mod(-30))  
-    streak_inc.pack(side = 'right', anchor='e', expand=True)
-    streak_dec.pack(side='right', anchor='w', expand=True)
+    streak_inc.pack(side = 'right', anchor='e', expand=False)
+    streak_dec.pack(side= 'right', anchor='w', expand=False)
 
 streak_icon = customtkinter.CTkLabel(bottom_frame, text="")
 streak_icon.pack(side='left', anchor='e', expand=True)
@@ -307,7 +352,6 @@ app.minsize(app.winfo_width(), app.winfo_height())
 
 # Run time update
 time_since_json_write = 0
-sort_labels()
 update_datetime_labels()
 t = threading.Thread(target=lambda: every(1, update_datetime_labels))
 t.daemon = True
